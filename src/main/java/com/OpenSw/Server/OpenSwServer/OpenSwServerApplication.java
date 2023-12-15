000b909247228db300af12885ba30503c370d851
programofktw <thkim000317@naver.com>
2023-12-15 17:45:25 +0900

    2023/12/15 kimtaewan project_Server_Start

---
+++ src/main/java/com/OpenSw/Server/OpenSwServer/OpenSwServerApplication.java
@@ -0,0 +1,13 @@
+package com.OpenSw.Server.OpenSwServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSwServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OpenSw/Server/OpenSwServer/OpenSwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OpenSw.Server.OpenSwServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSwServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
